79f05e184f0f4c385d25b5c2be9da572a4935633
Osmben <freegiftcardishere@gmail.com>
2023-03-18 00:08:30 +0100

    fixing  file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,12 @@ int main(void)
 	while (n <= '9')
 	{
 		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
