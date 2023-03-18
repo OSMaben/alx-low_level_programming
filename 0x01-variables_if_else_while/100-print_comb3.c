6fc26580b4511957b64427fc57ddaa865f601473
Osmben <freegiftcardishere@gmail.com>
2023-03-18 16:34:31 +0100

    adding 100 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - this program shows lowercase alphabets reversed
+ * Description - printing all possible different combinations of two digits.
+ * Return:0
+ */
+int main(void)
+{
+	int n = 0;
+
+	int m;
+
+	while (n <= 8)
+	{
+		m = 1;
+		while (m <= 9)
+		{
+			putchar(n + 48);
+			putchar(m + 48);
+			if (n != 8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
