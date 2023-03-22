c43f7a21f4faef584a66231fe386002fc4f23250
Osmben <freegiftcardishere@gmail.com>
2023-03-22 14:37:32 +0000

    adding 101 file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: computes and prints the sum of all the
+ * multiples of 3 or 5 below 1024 (excluded)
+ *
+ * Return: Alywasy 0 (Success)
+ */
+
+int main(void)
+{
+int num, sum;
+
+for (num = 0 ; num < 1024; num++)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+{
+sum += num;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
