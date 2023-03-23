72116295c77c980f5d2ebc4b0f49bf27d24c97c2
Osmben <freegiftcardishere@gmail.com>
2023-03-23 23:08:16 +0000

    adding 7 print diagonle file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,23 @@
 
 void print_diagonal(int n)
 {
-int a;
-for (a = 0; a < n; a++)
-{
-_putchar('\\');
+int x, y;
+
 if (n <= 0)
 {
 _putchar('\n');
 }
+else
+{
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < n; y++)
+{
+_putchar(32);
+}
+_putchar(92);
 _putchar('\n');
 }
-_putchar('\n');
+}
 
 }
