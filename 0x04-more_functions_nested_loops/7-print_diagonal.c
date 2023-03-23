147b86db0ddd05d31f6b9bc78466d6e1b9cdcf90
Osmben <freegiftcardishere@gmail.com>
2023-03-23 22:58:40 +0000

    adding 7 print diagonla file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - this program returns the diagonal
+ *
+ *  @n: character in ascii code
+ *
+ */
+
+void print_diagonal(int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+_putchar('\\');
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('\n');
+}
+_putchar('\n');
+
+}
