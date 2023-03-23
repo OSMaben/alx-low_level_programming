2d94a53913ae589a0f383bdbefafff0d751b7b24
Osmben <freegiftcardishere@gmail.com>
2023-03-23 07:50:17 +0000

    removing two file and fixing 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,29 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * positive_or_negative - intery point
- *
- * @i: this parameter takes diff value each time
- * the program is excuted
- * Description: this fucntion returns the either
- * a pos or nig value
- *
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
-void positive_or_negative(int i)
+int main(void)
 {
+int i;
 
-srand(time(0));
-i = rand() - RAND_MAX / 2;
+i = 0;
+positive_or_negative(i);
 
-if (i > 0)
-printf("%i is positive\n", i);
-else if (i == 0)
-printf("%i is zero\n", i);
-else
-printf("%i is negative\n", i);
-
+return (0);
 }
