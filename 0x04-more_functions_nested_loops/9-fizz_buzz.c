4585f4d14b77c30b9dc9155b17b02e9f83aba07d
Osmben <freegiftcardishere@gmail.com>
2023-03-24 13:16:00 +0000

    fixing again & again fizz buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,21 +19,20 @@ for (i = 1; i <= 100; i++)
 if (i % 15 == 0)
 {
 
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 if (i % 3 == 0)
 {
-printf("Fizz ");
+printf(" Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 
-printf("%d", i);
+printf(" %d", i);
 if (i < 100)
 {
-printf(" ");
 }
 
 }
