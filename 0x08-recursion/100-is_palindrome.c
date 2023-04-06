1802ef4e2904bd5d4268b0590a6f8fa8ea0eeee5
Osmben <freegiftcardishere@gmail.com>
2023-04-06 17:07:17 +0000

    fixing 100 is palindrom file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,22 @@
 #include "main.h"
 
+
 /**
- * is_palindrome_Child is a helper function that returns true
+ * string_length - function is a recursive function that
+ * takes a pointer to a string s and returns its length
+ *  @s: the string to be checked
+ * Return: integer
+ */
+
+int string_length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + string_length(s + 1));
+}
+/**
+ * is_palindrome_Child - is a helper function that returns true
  * if the child is a palette
  *  @s: the name of the child
  *  @left: the left coordinate of the child
@@ -10,22 +25,14 @@
  * Return: true if the child is a palette
  *
  */
-
-
 int is_palindrome_Child(char *s, int left, int right)
 {
 	if (left >= right)
-	{
 		return (1);
-	}
 	if (s[left] != s[right])
-	{
 		return (0);
-	}
 	return (is_palindrome_Child(s, left + 1, right - 1));
 }
-
-
 /**
  * is_palindrome - a function that returns 1 if a string is
  * a palindrome and 0 if not.
@@ -34,10 +41,7 @@ int is_palindrome_Child(char *s, int left, int right)
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
-
-	while (s[len] != '\0')
-		len++;
+	int len = string_length(s);
 
 	return (is_palindrome_Child(s, 0, len - 1));
 }
