cc0da1d5e50050ce3bb15524a61d65fa2f1b07ae
Osmben <freegiftcardishere@gmail.com>
2023-04-10 15:07:03 +0000

    pushing 3 mul file that takes the first number and second and multiplies them

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this program is multiplies two numbers.
+ *
+ * @argc: number of arguments
+ * @argv: arguments vector
+ *
+ * Return: 0 if success
+*/
+
+int main(__attribute__((unused))int argc, char **argv)
+{
+	int i, sum, num1, num2, j = 1;
+
+	if (argv[1] == NULL || argv[2] == NULL)
+	{
+		printf("Erorr\n");
+		return (1);
+	}
+	for (i = 0 ; i < j ; i++)
+	{
+		num1 =  atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		sum = num1 * num2;
+		printf("%d\n", sum);
+	}
+	return (0);
+}
