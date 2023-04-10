861514f37ae7ba48fa3b18d492eaf0ba479599de
Osmben <freegiftcardishere@gmail.com>
2023-04-10 12:00:52 +0000

    fixing the whats my name file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * main - function that prints the name of the fiile
+ * even if i change it
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if success
+*/
+
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
