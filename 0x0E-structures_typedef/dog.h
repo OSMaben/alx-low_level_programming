96e730ca0cab1112cfbfcd11bd0e45f5bb13dcb0
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-17 08:50:39 -0700

    fixing dog file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - struct that contains info about the dog
- * @name: name of the dog 
+ *
+ * @name: name of the dog
+ *
  * @age: age of the dog
+ *
  * @owner: the owner of the dog
  *
- * Description: this structure contains information of the dag
- * his name and age and the dog's owner.
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
