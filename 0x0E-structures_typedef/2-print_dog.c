96fa3dc495b5cd8da82d428cda732ed40f4a1e54
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-18 07:41:45 -0700

    adding the print dog file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog -function that prints struct dog
+ * @d: pointer to the structore dog wich holes the value
+ *
+ * Return: NORHINg
+ */
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+
+}
