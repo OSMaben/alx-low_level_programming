6ca7c210ff67ea157a8e0d3c21519f98fd46a859
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-18 11:44:33 -0700

    fixing the file again

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,17 +18,71 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = malloc(strlen(name) + 1);
-	strcpy(new_dog->name, name);
-	new_dog->owner = malloc(strlen(owner) + 1);
-	strcpy(new_dog->owner, owner);
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	new_dog->name = NULL;
+	new_dog->owner = NULL;
+
+	if (name == NULL || owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
+
+	new_dog->name = malloc(strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+
+	new_dog->owner = malloc(strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
+
 	new_dog->age = age;
 	return (new_dog);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
