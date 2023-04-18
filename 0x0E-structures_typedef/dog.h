d8d65ba06f2c433382ae3d2dec0078bf7cb3c107
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-18 08:47:27 -0700

    fix the errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,17 +11,16 @@
  * @owner:and his owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog dog_t;
 
 
 #endif
