c3313db6c69a2389053859e8637ad984f17baaee
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-21 07:38:32 -0700

    fixing the if condifion

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,11 +10,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int count = 0;
-	if (n == 0)
-		return (0);
+
 	va_list arglist;
 
 	va_start(arglist, n);
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n ; i++)
 	{
