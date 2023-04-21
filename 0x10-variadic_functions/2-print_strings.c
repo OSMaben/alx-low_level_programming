42e7b263ddf50d3a71668aedf17593bdf6cbd151
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-21 10:46:40 -0700

    fixing print string  aagain

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,29 +10,25 @@
  */
 
 
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-
+	char *str;
 	unsigned int i;
+	va_list ap;
 
-	va_start(args, n);
+	va_start(ap, n);
+	if (separator == NULL)
+		separator = "";
 
-	if (!n)
+	for (i = 0; i < n; i++)
 	{
-		printf("\n");
-		return;
-	}
-	for (i = 0; i < n ; i++)
-	{
-		printf("%s", va_arg(args, char*));
-
-		if (i != n - 1 && separator != NULL)
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
 			printf("%s", separator);
 	}
-	va_end(args);
-
+	printf("\n");
+	va_end(ap);
 }
-
-
