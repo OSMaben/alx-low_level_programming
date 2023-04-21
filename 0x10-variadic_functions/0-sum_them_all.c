ef61637efb7461d4be51578c4aea9521f885f172
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-04-21 07:23:42 -0700

    push the first file of the new project

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum them all the arguments
+ * @n: number of arguments
+ *
+ * Return: sum of all the arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int count = 0;
+
+	va_list arglist;
+
+	va_start(arglist, n);
+
+	for (i = 0; i < n ; i++)
+	{
+		count += va_arg(arglist, unsigned int);
+	}
+	va_end(arglist);
+
+	return (count);
+}
