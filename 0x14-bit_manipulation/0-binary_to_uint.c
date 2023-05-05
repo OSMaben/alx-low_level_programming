03b4f5fb185d15cf2d86c110e8d52854b76f9125
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-05-05 16:18:47 -0700

    trying to fix the first file again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,10 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int rst = 0;
 
-	if (b == NULL)
+	if (b)
 		return (0);
 
-	while (*b != 0)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
