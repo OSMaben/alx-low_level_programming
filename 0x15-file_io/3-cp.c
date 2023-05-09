bf77a37f1df4794b860503047d8dec21a951bc2c
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-05-09 13:01:36 -0700

    hope that it work

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,71 +1,41 @@
 #include "main.h"
 
-
-
-/**
- * handle_error - handle an error
- * @exit_code: exit with exit code
- * @error_msg: error message
- * @filename: file name
- * @fd: file descriptor
- * Return: nothing
-*/
-
-
-void handle_error(int exit_code, char *error_msg, char *filename, int fd)
-{
-	dprintf(STDERR_FILENO, error_msg, filename);
-
-	if (fd != -1)
-	{
-
-		close(fd);
-
-	}
-
-	exit(exit_code);
-}
-
-
 /**
  * main - main function
- * @argc: argc number
- * @argv: argments vector
+ * @argc: number of arguments
+ * @argv: arguments vector
  *
  * Return: 0 on success
 */
 
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to;
-	ssize_t num_read, num_written;
-	char buf[BUFFER_SIZE];
+int from_fd = 0, to_fd = 0;
+ssize_t b;
+char buf[BUFFER_SIZE];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error(98, "Error: Can't read from file %s\n", argv[1], -1);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
-			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to == -1)
-		handle_error(99, "Error: Can't write to %s\n", argv[2], fd_from);
-	while ((num_read = read(fd_from, buf, BUFFER_SIZE)) > 0)
-	{
-		num_written = write(fd_to, buf, num_read);
-		if (num_written == -1)
-			handle_error(99, "Error: Can't write to %s\n", argv[2], fd_from);
-		if (num_written != num_read)
-			handle_error(99, "Error: Failed to write to %s\n", argv[2], fd_from);
-	}
-	if (num_read == -1)
-		handle_error(98, "Error: Can't read from file %s\n", argv[1], fd_from);
-	if (close(fd_from) == -1)
-		handle_error(100, "Error: Can't close fd %d\n", argv[1], fd_from);
-	if (close(fd_to) == -1)
-		handle_error(100, "Error: Can't close fd %d\n", argv[2], fd_to);
-	return (0);
+if (argc != 3)
+dprintf(STDERR_FILENO, USAGE), exit(97);
+from_fd = open(argv[1], O_RDONLY);
+if (from_fd == -1)
+dprintf(STDERR_FILENO, ERR_READNO, argv[1]), exit(98);
+to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMS);
+if (to_fd == -1)
+dprintf(STDERR_FILENO, ERR_WRITENO, argv[2]), exit(99);
+
+while ((b = read(from_fd, buf, BUFFER_SIZE)) > 0)
+if (write(to_fd, buf, b) != b)
+dprintf(STDERR_FILENO, ERR_WRITENO, argv[2]), exit(99);
+if (b == -1)
+dprintf(STDERR_FILENO, ERR_READNO, argv[1]), exit(98);
+
+from_fd = close(from_fd);
+to_fd = close(to_fd);
+if (from_fd)
+dprintf(STDERR_FILENO, ERR_CLOSENO, from_fd), exit(100);
+if (to_fd)
+dprintf(STDERR_FILENO, ERR_CLOSENO, from_fd), exit(100);
+
+return (EXIT_SUCCESS);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,11 +11,16 @@
 
 
 #define BUFFER_SIZE 1024
-
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_READNO "Error: Can't read from file %s\n"
+#define ERR_WRITENO "Error: Can't write to %s\n"
+#define ERR_CLOSENO "Error: Can't close fd %d\n"
+#define PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
 
