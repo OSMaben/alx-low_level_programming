d2bf0c4132fc1065ea9bce462fa6db7e7a0c2fa3
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-05-09 09:41:23 -0700

    adding the file again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,39 +9,32 @@
  * Return:1 on success, -1 on failure
 */
 
-
 int create_file(const char *filename, char *text_content)
 {
-	int opn;
-	ssize_t num = 0, length = strlen(text_content);
+	int fd;
 
 	if (!filename)
 		return (-1);
 
-	opn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (opn == -1)
+	if (fd == -1)
+	{
 		return (-1);
+	}
 
-	if (length > 0)
+	if (text_content)
 	{
-		num = write(opn, text_content, length);
-		if (num != length)
+		size_t length = strlen(text_content);
+		ssize_t bytes_written = write(fd, text_content, length);
+
+		if (bytes_written == -1 || bytes_written != (ssize_t)length)
 		{
-			close(opn);
+			close(fd);
 			return (-1);
 		}
 	}
-	else
-	{
-		num = write(opn, "", 0);
-		if (num != 0)
-		{
-			close(opn);
-			return (-1);
-		}
 
-	}
-	close(opn);
+	close(fd);
 	return (1);
 }
