9b73f01750ba047c04d6eec82b34515ee130f1d5
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-05-09 13:29:51 -0700

    work please

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,9 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
-
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 #define USAGE "Usage: cp file_from file_to\n"
@@ -21,6 +23,15 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void elf_check(unsigned char *e_ident);
+void pmagic(unsigned char *e_ident);
+void pclass(unsigned char *e_ident);
+void pdata(unsigned char *e_ident);
+void pversion(unsigned char *e_ident);
+void pabi(unsigned char *e_ident);
+void posabi(unsigned char *e_ident);
+void ptype(unsigned int e_type, unsigned char *e_ident);
+void pentry(unsigned long int e_entry, unsigned char *e_ident);
+void celf(int elf);
 #endif
 
