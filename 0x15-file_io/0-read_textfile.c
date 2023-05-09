e620a4631a026a297a67c99814bb0c9047243299
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-05-09 07:36:53 -0700

    trying for the forth time

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * read_textfile - a function that reads a text file and prints
  * it to the POSIX standard output
@@ -8,23 +7,22 @@
  * @filename: the name of the file to read
  * @letters:  is the number of letters it should read and print
  *
- * Return:actual number of letters it could read and print
+ * Return: actual number of letters it could read and print
 */
 
-#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	ssize_t num;
-	char file_buffer[BUFFER_SIZE  * 8];
+	char file_buffer[BUFFER_SIZE];
 
 	if (!filename || !letters)
 		return (0);
 
 	file = open(filename, O_RDONLY);
-	num = read(file, &file_buffer[0], letters);
-	num = write(STDOUT_FILENO, &file_buffer[0], num);
+	num = read(file, file_buffer, letters);
+	num = write(STDOUT_FILENO, file_buffer, num);
 
 	if (file == -1 || num == -1)
 		return (0);
@@ -32,3 +30,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(file);
 	return (num);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
