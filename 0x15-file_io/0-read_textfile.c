d085880696ed4f850e2911afd2ee2c6a4530a00b
oussama ben mazzi <freegiftcardishere@gmail.com>
2023-05-09 07:24:27 -0700

    trying to fix again

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,30 +11,24 @@
  * Return:actual number of letters it could read and print
 */
 
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t opn, wrt, rd;
-	char *file_buffer;
+	int file;
+	ssize_t num;
+	char file_buffer[BUFFER_SIZE  * 8];
 
-	if (filename == NULL)
+	if (!filename || !letters)
 		return (0);
-	file_buffer = malloc(sizeof(char) * letters);
-	if (file_buffer == NULL)
-	{
+
+	file = open(filename, O_RDONLY);
+	num = read(file, &file_buffer[0], letters);
+	num = write(STDOUT_FILENO, &file_buffer[0], num);
+
+	if (file == -1 || num == -1)
 		return (0);
-	}
-	opn = open(filename, O_RDONLY);
-	rd = read(opn, file_buffer, letters);
-	wrt = write(STDOUT_FILENO, file_buffer, letters);
 
-	if (opn == -1 || wrt == -1 || rd == -1 || wrt != rd)
-	{
-		free(file_buffer);
-		return (0);
-	}
-
-	free(file_buffer);
-	close(opn);
-
-	return (wrt);
+	close(file);
+	return (num);
 }
